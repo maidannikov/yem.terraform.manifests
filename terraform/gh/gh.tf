2d51f25dd8116ebedc805e9d0ff2e8d85ab4774b
ShakeIT <89180289+Emaydannikov@users.noreply.github.com>
2023-08-09 13:31:42 +0300

    Update gh.tf

--- terraform/gh/gh.tf
+++ terraform/gh/gh.tf
@@ -81,3 +81,42 @@ resource "github_repository" "whalescan" {
     }
   }
 }
+
+resource "github_repository" "Simple_cv" {
+  allow_auto_merge                        = false
+  allow_merge_commit                      = true
+  allow_rebase_merge                      = true
+  allow_squash_merge                      = true
+  allow_update_branch                     = false
+  archive_on_destroy                      = null
+  archived                                = false
+  auto_init                               = false
+  delete_branch_on_merge                  = false
+  description                             = "Simple_CV_test"
+  gitignore_template                      = null
+  has_discussions                         = false
+  has_downloads                           = true
+  has_issues                              = true
+  has_projects                            = true
+  has_wiki                                = true
+  homepage_url                            = null
+  ignore_vulnerability_alerts_during_read = null
+  is_template                             = false
+  license_template                        = null
+  merge_commit_message                    = "PR_TITLE"
+  merge_commit_title                      = "MERGE_MESSAGE"
+  name                                    = "Simple_cv"
+  squash_merge_commit_message             = "COMMIT_MESSAGES"
+  squash_merge_commit_title               = "COMMIT_OR_PR_TITLE"
+  topics                                  = []
+  visibility                              = "public"
+  vulnerability_alerts                    = false
+  security_and_analysis {
+    secret_scanning {
+      status = "disabled"
+    }
+    secret_scanning_push_protection {
+      status = "disabled"
+    }
+  }
+}
