cf6e826e089eb3e488f6518055dadc264848d59a
ShakeIT <89180289+Emaydannikov@users.noreply.github.com>
2023-10-10 14:44:48 +0300

    Update py_proj.tf

--- terraform/gh/py_proj.tf
+++ terraform/gh/py_proj.tf
@@ -1,1 +1,33 @@
-
+resource "github_repository" "Riminder_bot" {
+  allow_auto_merge                        = false
+  allow_merge_commit                      = true
+  allow_rebase_merge                      = true
+  allow_squash_merge                      = true
+  allow_update_branch                     = false
+  archived                                = false
+  auto_init                               = false
+  delete_branch_on_merge                  = false
+  description                             = "RBot"
+  has_discussions                         = false
+  has_downloads                           = true
+  has_issues                              = true
+  has_projects                            = true
+  has_wiki                                = true
+  is_template                             = false
+  merge_commit_message                    = "PR_TITLE"
+  merge_commit_title                      = "MERGE_MESSAGE"
+  name                                    = "simple.python.telegrambot"
+  squash_merge_commit_message             = "COMMIT_MESSAGES"
+  squash_merge_commit_title               = "COMMIT_OR_PR_TITLE"
+  topics                                  = []
+  visibility                              = "public"
+  vulnerability_alerts                    = false
+  security_and_analysis {
+    secret_scanning {
+      status = "disabled"
+    }
+    secret_scanning_push_protection {
+      status = "disabled"
+    }
+  }
+}
