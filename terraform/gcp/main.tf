204aad4ab2e880c2383ebc5eb3f6ff192bedeaff
Emaydannikov <89180289+Emaydannikov@users.noreply.github.com>
2023-10-15 20:17:18 +0300

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,26 +1,27 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "py-bots"
-  machine_type = "f1-micro"
+name         = "py-bots"
+machine_type = "f1-micro"
 
-  boot_disk {
+boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
-
-  network_interface {
+network_interface {
+    # A default network is created for all GCP projects
     network = "default"
+    access_config {
+    }
   }
 
-  metadata_startup_script = <<-EOF
+metadata_startup_script = <<-EOF
 #!/bin/bash
 
-# Update and upgrade system packages
+# Install necessary packages
 sudo apt-get update
 sudo apt-get upgrade -y
-
-# Install necessary packages
-sudo apt-get install -y git python3-pip python3
+sudo apt-get install -y git
+sudo apt-get install -y pip
+sudo apt-get install -y python3-full
 
 # Clone the Telegram bot repository
 sudo git clone https://github.com/Emaydannikov/reminder.python.telegrambot.git /home/py-bot/
@@ -35,7 +36,7 @@ FILE_NAME = "notification.json"
 EOL
 
 # Install Python dependencies
-sudo pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
 
 # Create a systemd service file for the bot
 sudo tee /etc/systemd/system/my_telegram_bot.service > /dev/null <<EOL
@@ -46,7 +47,7 @@ After=network.target
 [Service]
 User=YOUR_USERNAME
 WorkingDirectory=/home/py-bot/
-ExecStart=/usr/bin/python3 /home/py-bot/main_bot_file.py
+ExecStart=/usr/bin/python3 /home/py-bot/bot.py
 Restart=always
 
 [Install]
