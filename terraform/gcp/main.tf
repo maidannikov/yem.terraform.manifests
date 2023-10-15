c43d96fc0a5ba6913dc8ce656dc99b5e32853ada
Emaydannikov <89180289+Emaydannikov@users.noreply.github.com>
2023-10-15 19:45:32 +0300

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -14,14 +14,48 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 metadata_startup_script = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get upgrade -y
-    apt-get install -y git
-    apt-get install -y pip
-    apt-get install -y python3-full
-    git clone https://github.com/Emaydannikov/reminder.python.telegrambot.git /home/py-bot/
-    cd /home/py-bot/
-    pip install -r requirements.txt
-    EOF
+   #!/bin/bash
+
+# Update and upgrade system packages
+apt-get update
+apt-get upgrade -y
+
+# Install necessary packages
+apt-get install -y git python3-pip python3
+
+# Clone the Telegram bot repository
+git clone https://github.com/Emaydannikov/reminder.python.telegrambot.git /home/py-bot/
+
+# Change directory to the bot's directory
+cd /home/py-bot/
+
+# Create config.py file with the specified content
+cat <<EOF > /home/py-bot/config.py
+TOKEN = "var.TBotTOKEN"
+FILE_NAME = "notification.json"
+EOF
+
+# Install Python dependencies
+pip3 install -r requirements.txt
+
+# Create a systemd service file for the bot
+cat <<EOF > /etc/systemd/system/my_telegram_bot.service
+[Unit]
+Description=My Telegram Bot Service
+After=network.target
+
+[Service]
+User=YOUR_USERNAME
+WorkingDirectory=/home/py-bot/
+ExecStart=/usr/bin/python3 /home/py-bot/main_bot_file.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start the bot service
+systemctl enable my_telegram_bot.service
+systemctl start my_telegram_bot.service
+
 }
