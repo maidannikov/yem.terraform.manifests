023944ec2744d55939d800a88d6fed8a49d1df79
Emaydannikov <89180289+Emaydannikov@users.noreply.github.com>
2023-10-16 12:50:51 +0300

    Update provider.tf

--- terraform/gcp/provider.tf
+++ terraform/gcp/provider.tf
@@ -10,6 +10,6 @@ terraform {
 provider "google" {
   credentials = var.GOOGLE_APPLICATION_CREDENTIALS
   project = "midyear-gift-395510" 
-  region  = "europe-east1"
-  zone    = "europe-east1-b"
+  region  = "europe-central2"
+  zone    = "europe-central2-a"
 }
