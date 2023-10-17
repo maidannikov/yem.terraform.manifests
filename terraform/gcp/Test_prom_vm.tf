6191502ca4da20449009a937253cc8b35a0d8d0b
Emaydannikov <89180289+Emaydannikov@users.noreply.github.com>
2023-10-17 15:59:47 +0300

    Update Test_prom_vm.tf

--- terraform/gcp/Test_prom_vm.tf
+++ terraform/gcp/Test_prom_vm.tf
@@ -16,3 +16,15 @@ resource "google_compute_instance" "vm_instance_1" {
 metadata = {
     ssh-keys = "emaydannikov:${var.GCPSSHKEYPUB}"}
 }
+
+resource "google_compute_firewall" "allow-prometheus-and-web" {
+  name    = "allow-prometheus-and-web"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443", "9090"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
