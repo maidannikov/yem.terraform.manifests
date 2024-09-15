185c4f0c26d65c9f53fbd1ea27bad6eb1e8216cf
Yevhen Maidannikov <yevhen.maidannikov@sitecore.com>
2024-09-15 17:24:29 +0200

    Added webapp fix depr values

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -13,26 +13,23 @@ resource "azurerm_container_registry" "acr" {
   tags = var.tags
 }
 
-resource "azurerm_app_service_plan" "webappplan" {
+resource "azurerm_service_plan" "webapp-sp" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.acr-rg.location
   resource_group_name = azurerm_resource_group.acr-rg.name
-  kind                = "Linux"
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-  reserved = true
+  os_type    = "Linux"
+  sku_name = "B1"
+  tags = var.tags
 }
 
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "webapp" {
   name                = var.web_app_name
   location            = azurerm_resource_group.acr-rg.location
   resource_group_name = azurerm_resource_group.acr-rg.name
-  service_plan_id     = azurerm_app_service_plan.webappplan.id
+  service_plan_id     = azurerm_service_plan.webapp-sp.id
 
   site_config {
-    linux_fx_version = "PYTHON|3.12"
+    linux_fx_version = "PYTHON|3.10"
   }
   tags = var.tags
 }
