97ebf7f22b7ef4c8c9102696be394ca58d3e7f51
Yevhen Maidannikov <yevhen.maidannikov@sitecore.com>
2024-09-15 19:31:12 +0200

    setting up the application

--- terraform/azure/webapp.tf
+++ terraform/azure/webapp.tf
@@ -15,12 +15,10 @@ resource "azurerm_linux_web_app" "webapp" {
 
   site_config {
     application_stack {
-      docker_image {
-        image_name       = "${azurerm_container_registry.acr.login_server}/${var.web_app_name}:latest"
-        registry_url     = azurerm_container_registry.acr.login_server
-        registry_username = azurerm_container_registry.acr.admin_username
-        registry_password = azurerm_container_registry.acr.admin_password
-      }
+      docker_registry_url = azurerm_container_registry.acr.login_server
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+      docker_image_name = "${azurerm_container_registry.acr.login_server}/${var.web_app_name}:latest"
     }
   }
 
