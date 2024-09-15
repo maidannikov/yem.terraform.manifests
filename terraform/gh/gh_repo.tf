74fed3189eade91b8ca6a04b81931b62b4c61492
Yevhen Maidannikov <yevhen.maidannikov@sitecore.com>
2024-09-15 13:28:54 +0200

    Create a new repo

--- terraform/gh/gh_repo.tf
+++ terraform/gh/gh_repo.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "Reusable_WF2" {
+resource "github_repository" "GH_TO_AZURE_SYNC" {
   allow_auto_merge                        = false
   allow_merge_commit                      = true
   allow_rebase_merge                      = true
@@ -7,7 +7,7 @@ resource "github_repository" "Reusable_WF2" {
   archived                                = false
   auto_init                               = false
   delete_branch_on_merge                  = false
-  description                             = "Executing commands via SSH reusable workflow"
+  description                             = "tst"
   has_discussions                         = false
   has_downloads                           = true
   has_issues                              = true
@@ -16,7 +16,7 @@ resource "github_repository" "Reusable_WF2" {
   is_template                             = false
   merge_commit_message                    = "PR_TITLE"
   merge_commit_title                      = "MERGE_MESSAGE"
-  name                                    = "workflow.ssh.execute"
+  name                                    = "yem.workflow.toazuresync"
   squash_merge_commit_message             = "COMMIT_MESSAGES"
   squash_merge_commit_title               = "COMMIT_OR_PR_TITLE"
   topics                                  = []
