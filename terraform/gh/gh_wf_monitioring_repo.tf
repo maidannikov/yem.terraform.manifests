e1b6ceea4ab50f5df50684d5a87bb449b0ed3ff3
Yevhen Maidannikov <yevhen.maidannikov@sitecore.com>
2024-09-16 09:53:57 +0200

    Added new repo for WF status page

---
+++ terraform/gh/gh_wf_monitioring_repo.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "GH_WF_MONITOR" {
+  allow_auto_merge            = false
+  allow_merge_commit          = true
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_update_branch         = false
+  archived                    = false
+  auto_init                   = false
+  delete_branch_on_merge      = false
+  description                 = "PY tool to show the list of gh workflows and their status"
+  has_discussions             = false
+  has_downloads               = true
+  has_issues                  = true
+  has_projects                = true
+  has_wiki                    = true
+  is_template                 = false
+  merge_commit_message        = "PR_TITLE"
+  merge_commit_title          = "MERGE_MESSAGE"
+  name                        = "yem.tool.wfmonitor"
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+  topics                      = []
+  visibility                  = "public"
+  vulnerability_alerts        = false
+  security_and_analysis {
+    secret_scanning {
+      status = "disabled"
+    }
+    secret_scanning_push_protection {
+      status = "disabled"
+    }
+  }
+}
