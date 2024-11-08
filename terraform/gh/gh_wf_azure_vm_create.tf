25fe76cef5356f975da5ce21baa054b80f41b9d8
Yevhen Maidannikov <89180289+maidannikov@users.noreply.github.com>
2024-11-08 10:42:04 +0200

    Fix the repo

--- terraform/gh/gh_wf_azure_vm_create.tf
+++ terraform/gh/gh_wf_azure_vm_create.tf
@@ -1,26 +1,17 @@
 resource "github_repository" "GH_WF_CREATE" {
+  name                        = "yem.workflow.azurevmcreate"
+  description                 = "Azure VM create"
+  visibility                  = "public"
   allow_auto_merge            = false
   allow_merge_commit          = true
   allow_rebase_merge          = true
   allow_squash_merge          = true
-  allow_update_branch         = false
-  archived                    = false
-  auto_init                   = true
   delete_branch_on_merge      = true
-  description                 = "Azure VM create"
-  has_discussions             = false
-  has_downloads               = true
+  auto_init                   = true
   has_issues                  = true
   has_projects                = true
   has_wiki                    = true
-  is_template                 = false
-  merge_commit_message        = "PR_TITLE"
-  merge_commit_title          = "MERGE_MESSAGE"
-  name                        = "yem.workflow.azurevmcreate"
-  squash_merge_commit_message = "COMMIT_MESSAGES"
-  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
-  topics                      = []
-  visibility                  = "public"
+  archived                    = false
   vulnerability_alerts        = false
   security_and_analysis {
     secret_scanning {
@@ -40,4 +31,10 @@ resource "github_branch" "master_wf_create" {
 resource "github_branch_default" "default_wf_create" {
   repository = github_repository.GH_WF_CREATE.name
   branch     = github_branch.master_wf_create.branch
+}
+
+resource "github_branch" "dev_wf_create" {
+  repository = github_repository.GH_WF_CREATE.name
+  branch     = "dev"
+  source_branch = github_branch.master_wf_create.branch 
 }
