a8b772f041eb10ecd6ca9291ef8dd42b358a9842
Yevhen Maidannikov <yevhen.maidannikov@sitecore.com>
2024-11-08 18:27:34 +0200

    terraform fmt

--- terraform/gh/provider.tf
+++ terraform/gh/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.2.2"
+    }
+  }
+}
+
 provider "github" {
   token = var.github_token
 }
